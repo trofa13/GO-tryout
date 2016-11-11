3c5fb07ff1023e4a90a1e8d0d98a9a868b57261f
Trofim <trofa13@inbox.ru>
2016-11-11 11:22:13 +0300

    implemented pointers

--- structs/main.go
+++ structs/main.go
@@ -8,6 +8,15 @@ type human struct {
 	age int
 }
 
+type robot struct {
+	name string
+	material string
+}
+
+type speaker interface {
+	speak() string
+}
+
 func (g human) speak() string {
 	if g.age < 60 {
 		return g.name + " tells some words of foolishness"
@@ -16,9 +25,19 @@ func (g human) speak() string {
 }
 
 func main() {
-	person1 := human{name: "Alex", age: 20}
-	person2 := human{name: "Anton", age: 70}
+	humansList := getHumans()
 
-	fmt.Println(person1.speak())
-	fmt.Println(person2.speak())
+	for _, person := range humansList {
+		fmt.Println(person.speak())
+	}
+}
+
+
+func getHumans() []*human {
+	person1 := &human{name: "Alex", age: 20}
+	person2 := &human{name: "Anton", age: 70}
+	person3 := &human{name: "Valera", age: 15}
+
+	list := []*human{person1, person2, person3}
+	return list
 }
